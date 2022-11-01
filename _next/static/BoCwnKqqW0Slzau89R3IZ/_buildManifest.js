5e02e9ae39c33a6859cc2b7c11f332567f71edde
Dodecahedr0x <Dodecahedr0x@users.noreply.github.com>
2022-11-01 21:00:38 +0000

    deploy: 3bddd4b899eca4dd78dbf4ad475fe60a6e10ac72

---
+++ _next/static/BoCwnKqqW0Slzau89R3IZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-599d3fce28b604cb.js","static/chunks/pages/index-14c553d5e6d86a14.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
