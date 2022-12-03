1b3e77e949d92988c116f4560d4f1f454a3bd4cc
Dodecahedr0x <Dodecahedr0x@users.noreply.github.com>
2022-12-03 21:32:58 +0000

    deploy: 73e750185957451ec30382b3f0934cf8829029cb

---
+++ _next/static/chunks/252f366e-798a1b07982415ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,h,n){n.d(h,{mvF:function(){return e}});var a=n(88357);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"}}]})(t)}}}]);
